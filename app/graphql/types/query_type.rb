0201186911e5e874596a796d5f316844910e67f7
Ros <Fedorovch>
2022-08-15 18:24:50 +0300

    Graphql setup

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,14 +4,33 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :github_full_name, String, 'Find github fullname by Username' do
+      argument :username, String
+    end
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :github_repos, [String], 'Find repos of given user' do
+      argument :username, String
+    end
+
+    def github_full_name(username:)
+      users_response = HTTParty.get("http://api.github.com/users/#{username}")
+
+      parsed_body = JSON.parse(users_response.body)
+
+      if parsed_body['message'] == 'Not Found'
+        'Not Found'
+      else
+        parsed_body['name']
+      end
+    end
+
+    def github_repos(username:)
+      repos_response = HTTParty.get("http://api.github.com/users/#{username}/repos")
+      parsed_body = JSON.parse(repos_response.body)
+
+      return [] if parsed_body.is_a?(Hash) && parsed_body.key?('message')
+
+      parsed_body.map { |repo| repo['name'] }
     end
   end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,20 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin 'application', preload: true
+pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
+pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin '@apollo/client', to: 'https://ga.jspm.io/npm:@apollo/client@3.6.7/index.js'
+pin '@wry/context', to: 'https://ga.jspm.io/npm:@wry/context@0.6.1/lib/context.esm.js'
+pin '@wry/equality', to: 'https://ga.jspm.io/npm:@wry/equality@0.5.3/lib/equality.esm.js'
+pin '@wry/trie', to: 'https://ga.jspm.io/npm:@wry/trie@0.3.2/lib/trie.esm.js'
+pin 'graphql', to: 'https://ga.jspm.io/npm:graphql@16.5.0/index.mjs'
+pin 'graphql-tag', to: 'https://ga.jspm.io/npm:graphql-tag@2.12.6/lib/index.js'
+pin 'optimism', to: 'https://ga.jspm.io/npm:optimism@0.16.1/lib/bundle.cjs.js'
+pin 'react', to: 'https://ga.jspm.io/npm:react@18.2.0/index.js'
+pin 'symbol-observable', to: 'https://ga.jspm.io/npm:symbol-observable@4.0.0/lib/index.js'
+pin 'ts-invariant', to: 'https://ga.jspm.io/npm:ts-invariant@0.10.3/lib/invariant.js'
+pin 'ts-invariant/process/index.js', to: 'https://ga.jspm.io/npm:ts-invariant@0.10.3/process/index.js'
+pin 'tslib', to: 'https://ga.jspm.io/npm:tslib@2.4.0/tslib.es6.js'
+pin 'zen-observable-ts', to: 'https://ga.jspm.io/npm:zen-observable-ts@1.2.5/module.js'
